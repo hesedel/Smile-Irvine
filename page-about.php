2e7b9e181391752195bc9b7802ed565fc8a1000b
Hesedel <hes@pajaroncreative.com>
2013-11-14 19:14:28 +1100

    updates

--- page-about.php
+++ page-about.php
@@ -15,17 +15,43 @@
 					<h2>Dr. Shawn Loo</h2>
 					<strong>How he became the amazing dentist he is today</strong>
 				</div>
-				...
+				<img class="border float-left" alt="Dr. Shawn Loo" src="img/stock-photos/shutterstock_15563044.jpg" style="width: 140px;">
+				<p>Dr. Shawn Loo received his undergraduate degree in Biology at the University of California at Irvine. While at UCI, he served as the academic chair for Lambda Theta Delta.</p>
+				<p>Dr. Loo next attended the nationally recognized University of Southern California School of Dentistry where he earned his Doctorate of Dental Surgery. After graduating, he then served part time on the USC Restorative Faculty teaching the art of crowns, bridges and cosmetic dentistry.</p>
+				<p>He has also been an instructor for one of the world's leading cosmetic centers that focuses on teaching other dentists how to perform smile makeovers.</p>
+				<div class="g-box">
+					<h3>America's Top Dentist</h3>
+					<p>In 2009, Dr. Loo was recognized as one of "America's Top Dentist". Recently, he was also nominated as one of the best general and cosmetic dentist in Orange and Los Angeles County by the "Best of LA" TV program. <a href="#">Watch Interview ›</a></p>
+				</div>
+				<p>Dr. Loo's commitment to excellence is evident by his ongoing efforts to receive certifications and continuing education in the latest advancements in dentistry. His main goal is to provide cutting edge dentistry including minimal preparation cosmetic dentistry, implant dentistry and invisalign orthodontics. <a href="#">See our Philosophy ›</a></p>
 			</div>
 			<aside class="g-aside">
-				...
+				<h3>Distinctive Dental</h3>
+				<p>4030 Barranca Pkwy, Suite 220<br>Irvine, CA 92604 <a href="#">Map ›</a></p>
+				<p>
+					<span><strong>P:</strong> 949.857.9220</span><br>
+					<span><strong>F:</strong> 949.857.4327</span>
+				</p>
+				<br>
+				<h3>What Customers Are Saying:</h3>
+				<p><span>&#8220;</span> Everyone is Awesome! Dr. Loo is great. He always takes the time to explain things and made me feel comfortable and informed. Everyone from the receptionist to the dentist made sure I had great experience. <span>&#8221;</span><br>- S.G.</p>
+				<a href="#">See More Testimonials ›</a>
 			</aside>
 		</div>
 	</div>
 
 	<footer class="g-footer">
 		<div class="container">
-			...
+			<div class="row-1">
+				<div class="col-1">
+					<strong>Our Philosophy</strong>
+					<p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. <a href="#">Link</a></p>
+				</div>
+				<div class="col-2">
+					<strong>Why We Are Unique</strong>
+					<p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. <a href="#">Link</a></p>
+				</div>
+			</div>
 		</div>
 	</footer>
 
--- page-technology.php
+++ page-technology.php
@@ -1,3 +1,89 @@
 <!-- PAGE TECHNOLOGY -->
+<div class="g-page" id="page-technology">
 
+	<header class="g-header">
+		<div class="container">
+			<h1>How the Magic <span>Happens</span></h1>
+			<a href="#">Make an Appointment</a>
+		</div>
+	</header>
+
+	<div class="g-body">
+		<div class="container">
+			<div class="g-main">
+				<div class="g-h2">
+					<h2>The Technology We Use</h2>
+					<strong>Staying ahead of the technology cure helps us bring the best treatments to you and your family</strong>
+				</div>
+
+				<div class="g-tiles">
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+					<div class="tile">
+						<div class="img"><img alt="" src="img/stock-photos/iStock_000016695317Medium.jpg"></div>
+						<strong>E4D/CAD Cam</strong>
+						<p>The quick brown fox jumps over the lazy dog. <a href="#">Link ›</a></p>
+					</div>
+				</div>
+
+			</div>
+			<aside class="g-aside">
+				<h3>Distinctive Dental</h3>
+				<p>4030 Barranca Pkwy, Suite 220<br>Irvine, CA 92604 <a href="#">Map ›</a></p>
+				<p>
+					<span><strong>P:</strong> 949.857.9220</span><br>
+					<span><strong>F:</strong> 949.857.4327</span>
+				</p>
+				<br>
+				<h3>What Customers Are Saying:</h3>
+				<p><span>&#8220;</span> Everyone is Awesome! Dr. Loo is great. He always takes the time to explain things and made me feel comfortable and informed. Everyone from the receptionist to the dentist made sure I had great experience. <span>&#8221;</span><br>- S.G.</p>
+				<a href="#">See More Testimonials ›</a>
+			</aside>
+		</div>
+	</div>
+
+	<footer class="g-footer">
+		<div class="container">
+			<div class="row-1">
+				<div class="col-1">
+					<strong>Our Philosophy</strong>
+					<p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. <a href="#">Link</a></p>
+				</div>
+				<div class="col-2">
+					<strong>Why We Are Unique</strong>
+					<p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. <a href="#">Link</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</div>
 <!-- PAGE TECHNOLOGY END -->
